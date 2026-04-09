5807f140011ff82dd7b19c045181ff99d171bd1b
nick <69575053+nickoncrack@users.noreply.github.com>
2026-04-09 15:26:49 +0300

    Optimize renderer using glyphs

--- renderer.c
+++ renderer.c
@@ -8,21 +8,32 @@
 SDL_Window *window;
 SDL_Renderer *renderer;
 TTF_Font *font;
+SDL_Texture *glyphs[256];
+SDL_Color palette[8] = {
+    {0,0,0},
+    {255,0,0},
+    {0,255,0},
+    {255,255,0},
+    {0,0,255},
+    {255,0,255},
+    {0,255,255},
+    {255,255,255}
+};
+
+int glyph_x, glyph_y;
 
 extern uint8_t *memory;
 
 // for 800x600 and 16x9 font, max 50 chars per line and 65 lines
 
-void put_at(int row, int col, char *text, SDL_Color color) {
-    SDL_Surface *s = TTF_RenderText_Blended(font, text, color);
-    SDL_Texture *t = SDL_CreateTextureFromSurface(renderer, s);
-    SDL_FreeSurface(s);
+void put_at(int row, int col, char *text, uint8_t color) {
+    SDL_Texture *t = glyphs[text[0]];
+    SDL_Color c = palette[color];
 
-    SDL_Rect dst = {row * 9, col * 16, 0, 0}; // x, y, w, h
-    SDL_QueryTexture(t, NULL, NULL, &dst.w, &dst.h);
+    SDL_Rect dst = {row * glyph_x, col * glyph_y, glyph_x, glyph_y}; // x, y, w, h
 
+    SDL_SetTextureColorMod(t, c.r, c.g, c.b);
     SDL_RenderCopy(renderer, t, NULL, &dst);
-    SDL_DestroyTexture(t);
 }
 
 // pixel format: color, character
@@ -32,21 +43,8 @@ void render_fb() {
             uint8_t ch    = memory[FB_START + (r * COLS + c) * 2 + 0];
             uint8_t color = memory[FB_START + (r * COLS + c) * 2 + 1];
 
-            SDL_Color sdl_color;
-            switch (color) {
-                case 0x00: sdl_color = (SDL_Color){0, 0, 0}; break;       // black
-                case 0x01: sdl_color = (SDL_Color){255, 0, 0}; break;     // red
-                case 0x02: sdl_color = (SDL_Color){0, 255, 0}; break;     // green
-                case 0x03: sdl_color = (SDL_Color){255, 255, 0}; break;   // yellow
-                case 0x04: sdl_color = (SDL_Color){0, 0, 255}; break;     // blue
-                case 0x05: sdl_color = (SDL_Color){255, 0, 255}; break;   // magenta
-                case 0x06: sdl_color = (SDL_Color){0, 255, 255}; break;   // cyan
-                case 0x07: sdl_color = (SDL_Color){255, 255, 255}; break; // white
-                default:   sdl_color = (SDL_Color){128, 128, 128}; break; // gray
-            }
-
             char text[2] = {ch, '\0'};
-            put_at(c, r, text, sdl_color);
+            put_at(c, r, text, color);
         }
     }
 }
@@ -89,4 +87,14 @@ void renderer_init() {
     );
     
     font = TTF_OpenFont("font/ibmvga16x9.ttf", 16);
+    TTF_SizeText(font, "A", &glyph_x, &glyph_y);
+
+    // printable characters
+    for (uint8_t i = 32; i <= 126; i++) {   
+        char text[2] = {i, '\0'};
+
+        SDL_Surface *s = TTF_RenderText_Blended(font, text, palette[7]);
+        glyphs[i] = SDL_CreateTextureFromSurface(renderer, s);
+        SDL_FreeSurface(s);
+    }
 }
